7e9d1d07d74cc0c41296146ade6043c2a99458c4
Sergey Tolmachov <s.tolmachov@icloud.com>
2016-05-04 15:32:28 +0300

    Summary edits
    - added «close» icon in M-menu;
    - fixed mobile view of «frontend» page;

--- js/logo.js
+++ js/logo.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$(".icon-menu").click(function() {
 		$(".header-menu-box2").toggleClass("fade-menu2");
+		$(".icon-menu").toggleClass("icon-menu-focus");
 	});
 });
--- js/web.js
+++ js/web.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$(".icon-menu").click(function() {
 		$(".header-menu-box2").toggleClass("fade-menu2");
+		$(".icon-menu").toggleClass("icon-menu-focus");
 	});
 });
 
