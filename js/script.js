9bd9a58ea9ede471c77a258c8f955551b80d4dd5
Sergey Tolmachov <s.tolmachov@icloud.com>
2016-08-10 00:40:44 +0300

    header

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-$(document).ready(function() {
-	$(".icon-menu").click(function() {
-		$(".header-menu-box2").toggleClass("fade-menu2");
-		$(".icon-menu").toggleClass("icon-menu-focus");
-	});
-});
+$('.dropdown-toggle').dropdown()
 
 
-
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 250) {
-	   	$(".avatar-text").fadeIn();
-	   	}
-	   	else {
-	    $(".avatar-text").fadeOut();
-      }
-});
+$('#myDropdown').on('show.bs.dropdown', function () {
+  // do something…
+})
