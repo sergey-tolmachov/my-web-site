3bd1bc99ad8e4c4d2d4afdfadb987ff95c88da88
Sergey Tolmachov <s.tolmachov@icloud.com>
2016-08-23 10:45:33 +0300

    my skills
    finished this block

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 //}, function() {
 //  $('.dropdown-menu').fadeOut(700);
 //});
+
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 915) {
+        //clearHeader, not clearheader - caps H
+        $(".animated").addClass("rotateIn show");
+    }
+//    
+//    else {
+//        $(".animated").addClass("hide");
+//    }
+}); //missing );
