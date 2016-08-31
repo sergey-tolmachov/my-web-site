47a64cd5cce24115fe5698927a2b4706084a7440
Sergey Tolmachov <s.tolmachov@icloud.com>
2016-09-01 00:08:43 +0300

    portfolio
    first setUp

---
+++ js/portfolio-script.js
@@ -0,0 +1,14 @@
+$('.dropdown-hover-item').hover(function() {
+  $('.dropdown-menu').fadeIn(450);
+}, function() {
+  $('.dropdown-menu').fadeOut(450);
+});
+
+$(document).ready(function() {
+  $(".header-container").addClass("animated fadeInDown");
+});
+
+
+$(document).ready(function() {
+  $(".preview").addClass("animated zoomIn");
+});
